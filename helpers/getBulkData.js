c14b4a6993feaa41f922253b9486698c7ba523c7
ArabyCode <ayoub.ouakkaha@gmail.com>
2021-01-07 17:10:31 +0100

    Downloads & Stores JSONL data, Next: Process the Data

--- helpers/getBulkData.js
+++ helpers/getBulkData.js
@@ -1,18 +1,140 @@
 import axios from 'axios';
+const { v4 } = require('uuid');
+const fs = require('fs');
+const Path = require('path');
+import { BULK_STATUS_QUERY, BULK_INIT_MUTATION } from './muations';
+import { addShop } from '../models/shops';
 
-const getData = (url) => {
-    axios
-        .get(url, { headers: { 'Access-Control-Allow-Origin': '*' } })
-        .then((res) => {
-            console.log('Result', res);
+/**
+ *
+ * @param {string} shop : Shop Origin
+ * @param {string} token : secret access token
+ * @param {Http2ServerResponse} resp : secret access token
+ * @description Handels sending the bulk request and retreive the bulk request data back from the shopify api
+ */
+const initBulkRequest = (shop, token) => {
+    return new Promise((resolve, reject) => {
+        const url = 'https://' + shop + '/admin/api/2021-01/graphql.json';
+        axios({
+            url: url,
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Shopify-Access-Token': token,
+            },
+            data: {
+                query: BULK_INIT_MUTATION,
+            },
         })
-        .catch((err) => {
-            console.log(err);
-        });
+            .then((res) => {
+                const userErrorsObject = res.data.data.bulkOperationRunQuery.userErrors;
+                const errorsChildrens = Object.keys(userErrorsObject);
+
+                if (errorsChildrens.length > 0) {
+                    res.data.data.bulkOperationRunQuery.userErrors.map((item) => {
+                        console.log(item.message);
+                    });
+                } else {
+                    console.log('\n[+] Init ID: ', res.data.data.bulkOperationRunQuery.bulkOperation.id);
+                }
+
+                resolve();
+            })
+            .catch((err) => {
+                reject('Error (GetBulkData: initBulkRequest: Axios.catch) : ' + err);
+            });
+    }).catch((err) => {
+        console.log('Error (GetBulkData: initBulkRequest: Promise.catch) : ', err.message ? err.message : err);
+    });
 };
 
-const processBulkData = () => {
-    console.log('processing bulkData');
+/**
+ *
+ * @param {string} shop : Shop Origin
+ * @param {string} token : secret access token
+ * @param {Http2ServerResponse} resp : secret access token
+ * @description Handels sending the bulk request and retreive the bulk request data back from the shopify api
+ */
+const bulkStatusQuery = (shop, token) => {
+    return new Promise((resolve, reject) => {
+        const apiUrl = 'https://' + shop + '/admin/api/2021-01/graphql.json';
+
+        // repeat with the interval of 2 seconds
+        try {
+            let timerId = setInterval(() => {
+                console.log(' >> 2 Second Interval Start');
+                axios({
+                    url: apiUrl,
+                    method: 'post',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Shopify-Access-Token': token,
+                    },
+                    data: {
+                        query: BULK_STATUS_QUERY,
+                    },
+                })
+                    .then((res) => {
+                        const url = res.data.data.currentBulkOperation.url;
+
+                        if (url) {
+                            clearInterval(timerId);
+                            console.log('[+] Found Url & Cleared the Interval');
+                            resolve(url);
+                        }
+                        // else {
+                        //     console.log('\n \n \n [-] Didnt find the url yet, Intervalling again ', url);
+                        // }
+                    })
+                    .catch((err) => {
+                        console.log('Line 46 inside the bulk query status ' + err);
+                    });
+            }, 2000);
+        } catch (err) {
+            console.log('Line 50 try catch inside the bulk query status ' + err);
+            reject(err);
+        }
+    }).catch((err) => {
+        console.log('Line 50 try catch inside the bulk query status ' + err);
+    });
 };
 
-export default getData;
+/**
+ *
+ * @param {string} jsonlURL : Link to the actual JSONL file to be stored
+ * @returns {string} path : Path to the stored jsonl file
+ * @description Downloads and stores the JSONL file
+ */
+const downloadJSONL = (jsonlURL) => {
+    return new Promise((resolve, reject) => {
+        if (!jsonlURL) reject('500 Error, No valid url was passed to the server.');
+
+        const fileName = v4() + '.JSONL';
+        const path = Path.resolve(global.appRoot, 'files', fileName);
+
+        axios({
+            method: 'get',
+            url: jsonlURL,
+            responseType: 'stream', // important
+        })
+            .then((response) => {
+                response.data.pipe(fs.createWriteStream(path));
+
+                response.data.on('end', () => {
+                    resolve(path);
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                reject(err);
+            });
+    }).catch((err) => {
+        console.log('Error (GetBulkData: DownloadJSONL: .Promise.Catch): ', err);
+    });
+};
+
+module.exports = {
+    initBulkRequest,
+    bulkStatusQuery,
+    downloadJSONL,
+};
--- pages/api/shopify/index.js
+++ pages/api/shopify/index.js
@@ -1,102 +1,6 @@
-import axios from 'axios';
-const { v4 } = require('uuid');
-const fs = require('fs');
-const Path = require('path');
 var jwt = require('jsonwebtoken');
 const shops = require('../../../models/shops');
-import { BULK_STATUS_QUERY, BULK_INIT_MUTATION } from '../../..//helpers/muations';
-import { addShop } from '../../../models/shops';
-
-/**
- *
- * @param {string} shop : Shop Origin
- * @param {string} token : secret access token
- * @param {Http2ServerResponse} resp : secret access token
- * @description Handels sending the bulk request and retreive the bulk request data back from the shopify api
- */
-const bulkStatusQuery = (shop, token) => {
-    return new Promise((resolve, reject) => {
-        const apiUrl = 'https://' + shop + '/admin/api/2021-01/graphql.json';
-
-        // repeat with the interval of 2 seconds
-        try {
-            let timerId = setInterval(() => {
-                console.log('New Interval');
-                axios({
-                    url: apiUrl,
-                    method: 'post',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-Shopify-Access-Token': token,
-                    },
-                    data: {
-                        query: BULK_STATUS_QUERY,
-                    },
-                })
-                    .then((res) => {
-                        const url = res.data.data.currentBulkOperation.url;
-                        console.log('\n \n \n >>> the url is ');
-                        if (url) {
-                            clearInterval(timerId);
-                            console.log('[+] Cleared the Interval');
-                            resolve(url);
-                        }
-                    })
-                    .catch((err) => {
-                        console.log('Line 46 inside the bulk query status ' + err);
-                    });
-            }, 10000);
-        } catch (err) {
-            console.log('Line 50 try catch inside the bulk query status ' + err);
-            reject(err);
-        }
-    }).catch((err) => {
-        console.log('Line 50 try catch inside the bulk query status ' + err);
-    });
-};
-
-/**
- *
- * @param {string} shop : Shop Origin
- * @param {string} token : secret access token
- * @param {Http2ServerResponse} resp : secret access token
- * @description Handels sending the bulk request and retreive the bulk request data back from the shopify api
- */
-const initBulkRequest = (shop, token) => {
-    return new Promise((resolve, reject) => {
-        const url = 'https://' + shop + '/admin/api/2021-01/graphql.json';
-        axios({
-            url: url,
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Shopify-Access-Token': token,
-            },
-            data: {
-                query: BULK_INIT_MUTATION,
-            },
-        })
-            .then((res) => {
-                const userErrorsObject = res.data.data.bulkOperationRunQuery.userErrors;
-                const errorsChildrens = Object.keys(userErrorsObject);
-
-                if (errorsChildrens.length > 0) {
-                    res.data.data.bulkOperationRunQuery.userErrors.map((item) => {
-                        console.log(item.message);
-                    });
-                } else {
-                    console.log('\n[+] Init ID: ', res.data.data.bulkOperationRunQuery.bulkOperation.id);
-                }
-
-                resolve();
-            })
-            .catch((err) => {
-                reject('Axios.catch init bulk request errro : -> ' + err);
-            });
-    }).catch((err) => {
-        console.log('Init bulk request error : ', err.message ? err.message : err);
-    });
-};
+const getBulkData = require('../../../helpers/getBulkData');
 
 /**
  * @description Gets called when the user submites the tempalate form
@@ -135,14 +39,24 @@ export default async function templateForm(req, res) {
                          * here
                          */
 
-                        initBulkRequest(decoded.shop_origin, decoded.access_token)
-                            .then((url) => {
-                                bulkStatusQuery(decoded.shop_origin, decoded.access_token)
-                                    .then((myData) => {
-                                        console.log('Data gg .. ');
+                        getBulkData
+                            .initBulkRequest(decoded.shop_origin, decoded.access_token)
+                            .then((operationId) => {
+                                getBulkData
+                                    .bulkStatusQuery(decoded.shop_origin, decoded.access_token)
+                                    .then((jsonlURL) => {
+                                        console.log('Downloading the JSONL url');
+                                        getBulkData
+                                            .downloadJSONL(jsonlURL)
+                                            .then((jsonlPath) => {
+                                                console.log('Download: ', jsonlPath);
+                                            })
+                                            .catch((err) => {
+                                                reject('Error, Index(downloadJSONL.catch): ' + err);
+                                            });
                                     })
                                     .catch((err) => {
-                                        console.log('rer', err);
+                                        reject('Error, Index(BulkStatusQuery.catch): ' + err);
                                     });
                             })
                             .catch((err) => {
